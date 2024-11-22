2a4f0c9cbca4c2e20c6a7caab021c95c4684b643
root <root@ip-172-31-26-140.ap-southeast-1.compute.internal>
2024-11-22 10:56:37 +0000

    Initial commit

--- build.sh
+++ build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-# Build the Docker image
-docker build -t devops-build .
-
-# Tag the image with the version
-docker tag devops-build prabadevops1003/dev:latest
+echo "Building Docker image..."
+docker build -t devops-app .
 
--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-
-# Pull the image from DockerHub
-docker pull prabadevops1003/dev:latest
-
-# Stop any running container
-docker stop dev-app
-docker rm dev-app
-
-# Run the container
-docker run -d -p 80:80 --name dev-app prabadevops1003/dev:latest
+echo "Deploying Docker container..."
+docker-compose up -d
 
